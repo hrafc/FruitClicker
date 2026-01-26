89b17c8c758a89c6ecef6d835c0f6323c35bc3a6
hrafc <otagillug@gmail.com>
2026-01-26 20:04:40 +0100

    sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,32 @@
-const CACHE_NAME = "fruit-clicker-v1";
+const CACHE_NAME = "fruit-clicker-v3";
+
 const FILES = [
-  "./",
-  "./index.html",
-  "./manifest.json"
+  "/FruitClicker/",
+  "/FruitClicker/index.html",
+  "/FruitClicker/manifest.json"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(k => k !== CACHE_NAME && caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(
-      response => response || fetch(event.request)
-    )
+    caches.match(event.request).then(res => res || fetch(event.request))
   );
 });
+
