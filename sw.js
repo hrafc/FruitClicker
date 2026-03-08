3ffdff48293c9a27e93aceae4739015181658093
hrafc <otagillug@gmail.com>
2026-03-08 01:07:58 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "fruit-clicker-v98";
+const CACHE_NAME = "fruit-clicker-v99";
 
 const FILES = [
   "/",
@@ -50,6 +50,7 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
 
 
 
